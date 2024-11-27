cd3f429b2253a13532c884e09c9c65fa0d1f57e7
Theng <thengpy05@gmail.com>
2024-11-27 23:32:31 +0800

    update

--- src/components/CafeteriaList.js
+++ src/components/CafeteriaList.js
@@ -5,14 +5,7 @@ import ad1 from '../img/ad1.jpg';
 const CafeteriaList = ({ cafeterias, onSelect }) => {
   return (
     <div className="cafeteria-list">
-      <h2>Welcome Back!</h2>
-      <p style = {{color: "black"}}>Start shopping for unbeatable deals on food you love. </p>
-      <div className = "ads">
-        <img src = {ad1} />
-        <img src = {ad1} />
-        <img src = {ad1} />
-      </div>
-      <h3>Start ordering now!</h3>
+      
       <div className="cafeteria-option">
         {cafeterias.map(cafeteria => (
           <div key={cafeteria.id} className="cafeteria-item" onClick={() => onSelect(cafeteria.id)}>
