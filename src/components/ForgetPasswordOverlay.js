a37d11cd193770b2f3d54b6c07f554bdbd7ac206
Jocelyn Gan <24054433@siswa.um.edu.my>
2024-11-27 23:59:38 +0800

    Fixed ForgetPassword and Overlay exit button

--- src/components/ForgetPasswordOverlay.js
+++ src/components/ForgetPasswordOverlay.js
@@ -1,21 +1,22 @@
 import React from 'react';
-
+import './SignUpPage.css';
+import forget_pw from '../img/forgetpassword.png';
 
 const ForgetPasswordOverlay = ({ onClose, onSubmit, email, setEmail }) => {
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="close-btn" onClick={onClose}>X</button>
+                <div className = "close-btn">
+                    <span class="material-symbols-rounded" onClick={onClose}>close</span>
+                </div>
                 <h2>Forget Password</h2>
+                <div className = "img-wrapper"><img className = "signup-img" src = {forget_pw} /></div>
                 <form onSubmit={onSubmit} className="sign-up-form">
-                    <div>
-                        <label>Email:</label>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
+                    <div className = "input-container">
+                        <div className = "input-wrapper">
+                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder = "Enter Email" required />
+                            <i className="material-symbols-rounded">email</i>
+                        </div>
                     </div>
                     <button type="submit" className="submit-btn">Submit</button>
                 </form>
