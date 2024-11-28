1bc0250412fb31e1a6e300548e2297906447eca7
Kest-3r <kest3r.05@gmail.com>
2024-11-28 19:21:56 +0800

    update

--- src/Server.js
+++ src/Server.js
@@ -170,6 +170,18 @@ app.get('/comments', (req, res) => {
         });
 });
 
+//Route to payments
+app.post('/payment', (req, res) => {
+    const { username, price } = req.body;
+
+    if(!username || !price) {
+        return res.status(400).send('Log in is required.');
+    }
+
+    console.log(`Processing payment for user: ${username}, price: ${price}`);
+    res.status(200).json({ success: true, message: 'Payment successful' });
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
