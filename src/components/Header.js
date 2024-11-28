235cf171b2d3e93979b66b8ed471ff965fd8be9f
Jocelyn Gan <24054433@siswa.um.edu.my>
2024-11-28 15:56:17 +0800

    fixed hamburger menu in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import '../styles.css';
-import logo from '../img/logo.png';
-//import mealplannerIcon from '../img/mealplanner.png'; // Adjust the path as necessary
-import ad1 from '../img/ad1.jpg'; // Ensure this is the correct path
+import ad1 from '../img/ad1.jpg'; 
+import HamburgerMenu from './HamburgerMenu';
 
 function Header({ onCartClick, onOrderHistoryClick, onProfileClick, onMealPlannerClick, onCommunityBoardClick }) {
     return (
         <>
             <header> 
                 <div className="logo"></div>
-                <div className="right-icons">  
-                    <span className="material-symbols-rounded" onClick={onCartClick}>shopping_bag</span>
-                    <span className="material-symbols-rounded" onClick={onOrderHistoryClick}>history</span>
-                    <span className="material-symbols-rounded" onClick={onMealPlannerClick}>smart_toy</span>
-                    <span className="material-symbols-rounded" onClick={onCommunityBoardClick}>forum</span>
-                    <span className="material-symbols-rounded" onClick={onProfileClick}>person</span>
-                    </div>
+                <HamburgerMenu 
+                    onCartClick = {onCartClick}
+                    onOrderHistoryClick = {onOrderHistoryClick}
+                    onProfileClick = {onProfileClick}
+                    onMealPlannerClick = {onMealPlannerClick}
+                    onCommunityBoardClick = {onCommunityBoardClick}
+                />
             </header>
 
             <main>
